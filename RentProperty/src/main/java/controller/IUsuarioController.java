dd7cca344e7fb8da0909b9d081c607fc6e581abf
jesus2594 <jesusdavid890@hotmail.com>
2022-09-16 19:40:02 -0500

    Se agrega front con login y registro

---
+++ RentProperty/src/main/java/controller/IUsuarioController.java
@@ -0,0 +1,16 @@
+
+package controller;
+
+import java.util.Map;
+
+public interface IUsuarioController {
+    
+    public String login(String usuario, String contrasena);
+    
+    public String register(int idUsuario, String usuario, String contrasena, String nombre, String apellido, int telefono, String email,  String ciudad);
+    
+  
+    
+    
+    
+}
